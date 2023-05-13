10a1b6bc5632717f60d585f310cfd3ad18e5bb03
Chandan1035 <lchandanrao01@gmail.com>
2023-05-13 21:37:46 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,38 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.filter((student) => student.marks > 50).map((student) => console.log(student));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = { id: 4, name: "sai", age: "23", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((student) => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray = [
+     { id: 4, name: "raoy", age: "19", marks: 50 },
+     { id: 5, name: "shyam", age: "21", marks: 85 },
+     { id: 6, name: "zier", age: "20", marks: 39 },
+  ];
+  arr = arr.concat(newArray);
+  console.log(arr);
 }
